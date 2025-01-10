caf6669a5871b292b575f4c4166d0d022f629162
Yash Varma <71536409+yash509@users.noreply.github.com>
2025-01-10 17:52:16 +0530

    Add files via upload

---
+++ src/components/Team.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Junior Council</h2>
+          <p>
+          Meet the SR DTU team: a dynamic group of innovators committed to excellence and driven by a shared passion for technology and progress. Together, we turn visionary ideas into impactful solutions.
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img" />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/testimonials.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Testimonials = (props) => {
+  return (
+    <div id="testimonials">
+      <div className="container">
+        <div className="section-title text-center">
+          <h2>Senior Council</h2>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  <div className="council">
+                    <div className="testimonial-image">
+                      {" "}
+                      <img src={d.img} alt="" />{" "}
+                    </div>
+                    <div className="council-content">
+                      <p>"{d.text}"</p>
+                      <div className="council-meta"> - {d.name} </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
